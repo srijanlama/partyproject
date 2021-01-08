964c4922e4e32038586eebe4f5afb921f1de5f05
unknown <srijanlama81@gmail.com>
2021-01-07 19:54:48 -0800

    changes in gallary

--- js/main.js
+++ js/main.js
@@ -26,3 +26,20 @@ $( ".bookmark-link" ).click(function(e) {
   console.log( "You clicked a paragraph!" );
   $(".bookmark i").toggleClass('make-red');
 });
+//gallary script 
+jQuery(document).ready(function($) {
+  $('.image img').click(function(event) {
+
+    // detect data-id for later
+    var id = $(this).data('id');
+    // grab src to replace #featured
+    var src = $(this).attr('src');
+    // set featured image
+    var img = $('#featured img');
+
+    img.fadeOut('fast', function() {
+      $(this).attr({src: src,});
+      $(this).fadeIn('fast');
+    });
+  });
+});
